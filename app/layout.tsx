275c4e5c269889b0adf3f0cac0e26f906998e149
Yamato <kuma.y.0506@gmail.com>
2026-08-17 13:21:51 +1000

    fix: suppress extension hydration warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full antialiased">
+    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
       <body className="min-h-full flex flex-col bg-stone-100 text-stone-900">
         {children}
       </body>
